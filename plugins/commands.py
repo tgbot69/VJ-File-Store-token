79183aa6f514ddb11e66bb74accc45c96fa7a89a
tgbot69 <kd142047@gmail.com>
2024-09-16 17:07:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -128,11 +128,11 @@ async def start(client, message):
             os.remove(file)
             BATCH_FILES[file_id] = msgs
             
-        filesarr = []
-        for msg in msgs:
-            title = msg.get("title")
-            size=get_size(int(msg.get("size", 0)))
-            f_caption=msg.get("caption", "")
+    #    filesarr = []
+     #   for msg in msgs:
+      #      title = msg.get("title")
+       #     size=get_size(int(msg.get("size", 0)))
+        #    f_caption=msg.get("caption", "")
            # if BATCH_FILE_CAPTION:
               #  try:
              #       f_caption=BATCH_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
